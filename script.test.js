82e1a95dfa1a337e75ccda97b1c90c4f48bc05b1
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-16 10:16:24 +1100

    Finish testing analyzeArray

--- script.test.js
+++ script.test.js
@@ -8,3 +8,26 @@ test("Average 2.5, min 1, max 4, length 4", () => {
     length: 4,
   });
 });
+
+test("Large range", () => {
+  expect(analyzeArray([2384, 2, 284, 18492])).toEqual({
+    average: 5290.5,
+    min: 2,
+    max: 18492,
+    length: 4,
+  });
+});
+
+test("Large length", () => {
+  expect(
+    analyzeArray([
+      10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160,
+      170, 180,
+    ])
+  ).toEqual({
+    average: 95,
+    min: 10,
+    max: 180,
+    length: 18,
+  });
+});
