d595cc0dea3981fd8ba70cdeab9672ee5a6d60d4
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 15:20:45 +1100

    Test add method

--- script.test.js
+++ script.test.js
@@ -1,1 +1,13 @@
 import { calc } from "./script.js";
+
+test("Add 1 + 1", () => {
+  expect(calc.add(1, 1)).toBe(2);
+});
+
+test("Negative number", () => {
+  expect(calc.add(-55, 66)).toBe(11);
+});
+
+test("Two negative numbers", () => {
+  expect(calc.add(-652, -632)).toBe(-1284);
+});
