fe6704320c3dc8982b4f3d094b434a489698f243
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 15:26:21 +1100

    Test subtract method

--- script.test.js
+++ script.test.js
@@ -11,3 +11,15 @@ test("Negative number", () => {
 test("Two negative numbers", () => {
   expect(calc.add(-652, -632)).toBe(-1284);
 });
+
+test("Subtract 55 - 44", () => {
+  expect(calc.subtract(55, 44)).toBe(11);
+});
+
+test("Subtract negative number", () => {
+  expect(calc.subtract(-32, 12)).toBe(-44);
+});
+
+test("Subtract two negative numbers", () => {
+  expect(calc.subtract(-34, -45)).toBe(11);
+});
