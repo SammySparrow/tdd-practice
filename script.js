6928276d5ba308f88f57a48a1eab38f489db6bd8
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 14:46:40 +1100

    Import reverseString

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-export { capitalise };
+export { capitalise, reverseString };
 
 function capitalise(str) {
   const i = str.slice(0, 1).toUpperCase();
   const j = str.slice(1).toLowerCase();
   return i.concat("", j);
 }
+
+function reverseString() {}
--- script.test.js
+++ script.test.js
@@ -1,17 +1,5 @@
-import { capitalise } from "./script.js";
+import { reverseString } from "./script.js";
 
 test("All lower case", () => {
-  expect(capitalise("hello")).toBe("Hello");
-});
-
-test("All upper case", () => {
-  expect(capitalise("UPPER")).toBe("Upper");
-});
-
-test("Mixed upper and lower", () => {
-  expect(capitalise("MiXeD")).toBe("Mixed");
-});
-
-test("Symbols", () => {
-  expect(capitalise("it's working?")).toBe("It's working?");
+  expect(reverseString()).toBe("Hello");
 });
