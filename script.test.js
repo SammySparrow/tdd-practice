9056d402942ccc571b7b5cff07c872ce94dbbfd6
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 16:12:21 +1100

    Write cipher tests

--- script.test.js
+++ script.test.js
@@ -3,3 +3,23 @@ import { caeserCipher } from "./script.js";
 test("abc => def", () => {
   expect(caeserCipher("abc")).toBe("def");
 });
+
+test("xyz => abc", () => {
+  expect(caeserCipher("xyz")).toBe("abc");
+});
+
+test("Mixed upper and lower case: HeLLo => KhOOr", () => {
+  expect(caeserCipher("HeLLo")).toBe("KhOOr");
+});
+
+test("Punctuation: Hello, World! => Khoor, Zruog!", () => {
+  expect(caeserCipher("Hello, World!")).toBe("Khoor, Zruog!");
+});
+
+test("Shift increased by 3: abc => ghi", () => {
+  expect(caeserCipher("abc", 6)).toBe("ghi");
+});
+
+test("Shift increase and wrapping: xyz => def", () => {
+  expect(caeserCipher("xyz", 6)).toBe("def");
+});
