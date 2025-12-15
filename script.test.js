f94608a782ba2e1013128c6b9f102920f73e5e95
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 14:56:06 +1100

    Test reverseString fuction

--- script.test.js
+++ script.test.js
@@ -3,3 +3,11 @@ import { reverseString } from "./script.js";
 test("Hello to olleH", () => {
   expect(reverseString("Hello")).toBe("olleH");
 });
+
+test("Symbols", () => {
+  expect(reverseString("Get! Up! Get! Down?")).toBe("?nwoD !teG !pU !teG");
+});
+
+test("Numbers", () => {
+  expect(reverseString("123456789")).toBe("987654321");
+});
