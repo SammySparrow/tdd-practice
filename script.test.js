ce64ec463af2e74d1f2c9853d1c9b0d23da464c7
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-12-15 14:31:10 +1100

    Finish testing capitalise function

--- script.test.js
+++ script.test.js
@@ -1,5 +1,17 @@
 import { capitalise } from "./script.js";
 
-test("hello to Hello", () => {
+test("All lower case", () => {
   expect(capitalise("hello")).toBe("Hello");
+});
+
+test("All upper case", () => {
+  expect(capitalise("UPPER")).toBe("Upper");
+});
+
+test("Mixed upper and lower", () => {
+  expect(capitalise("MiXeD")).toBe("Mixed");
+});
+
+test("Symbols", () => {
+  expect(capitalise("it's working?")).toBe("It's working?");
 });
